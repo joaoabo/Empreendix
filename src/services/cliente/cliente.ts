52fd28f28ffea70f00148d9e75f3fbda5bf8b36e
joaoabo <82650085+joaoabo@users.noreply.github.com>
2025-04-26 10:12:56 -0300

    Melhorado services cliente com trycatch

--- src/services/cliente/cliente.ts
+++ src/services/cliente/cliente.ts
@@ -1,34 +1,45 @@
-import { db } from '../../libs/prisma';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { db } from "../../libs/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export const cadastrarCliente = async (clienteData: any) => {
-    const cliente = await db.cliente.create({
-        data: clienteData,
-    })
-    return cliente;
-}
+  try {
+    return await db.cliente.create({
+      data: clienteData,
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
+      return null;
+    }
+    throw err;
+  }
+};
 
 export const alterarCliente = async (Cliente_Id: number, campos: any) => {
-    const cliente = await db.cliente.update({
-        where: { Cliente_Id },
-        data: {
-            ...campos,
-            Data_atualizacao: new Date(),
-        },
-    })
-    return cliente;
-}
+  try {
+    return await db.cliente.update({
+      where: { Cliente_Id },
+      data: {
+        ...campos,
+        Data_atualizacao: new Date(),
+      },
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError && err.code === "P2025") {
+      return null;
+    }
+    throw err;
+  }
+};
 
 export const deletarCliente = async (Cliente_Id: number) => {
-    try {
-      const cliente = await db.cliente.delete({
-        where: { Cliente_Id },
-      });
-      return cliente;
-    } catch (err) {
-      if (err instanceof PrismaClientKnownRequestError && err.code === 'P2025') {
-        return null;
-      }
-      throw err;
+  try {
+    return await db.cliente.delete({
+      where: { Cliente_Id },
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError && err.code === "P2025") {
+      return null;
     }
-  };
+    throw err;
+  }
+};
