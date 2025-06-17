a05c75516be08d9f2923ecd9814097f77b9c282d
joaoabo <82650085+joaoabo@users.noreply.github.com>
2025-06-17 12:12:56 -0300

    Criado tela de login

--- Empreendix.Mobile/src/screens/controleUsuario/Login.tsx
+++ Empreendix.Mobile/src/screens/controleUsuario/Login.tsx
@@ -1,10 +1,46 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Text, TextInput, Button } from 'react-native-paper';
 import { styles } from './stylesUsuario'
+import Logo from '../../components/logo/Logo';
 
-export default function Login() {
+export default function Login({ }) {
+  const [email, setEmail] = useState('');
+  const [nome , setNome] = useState('');
+
+  const handleNext = () => {
+    return 'teste'
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Login!</Text>
+      <Logo />
+      <Text style={styles.title}>Bem-vindo 👋</Text>
+      <Text style={styles.subtitle}>Digite seu Nome</Text>
+
+      <TextInput
+        label="Digite seu nome"
+        value={nome}
+        onChangeText={setNome}
+        style={styles.input}
+      />
+      <Text style={styles.subtitle}>Digite seu e-mail para entrar</Text>
+      <TextInput
+        label="Seu e-mail"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+        keyboardType="email-address"
+      />
+
+      <Button
+        mode="contained"
+        onPress={handleNext}
+        style={styles.button}
+        disabled={!email.includes('@')}
+      >
+        Próximo
+      </Button>
     </View>
   );
 }
--- Empreendix.Mobile/src/screens/controleUsuario/stylesUsuario.tsx
+++ Empreendix.Mobile/src/screens/controleUsuario/stylesUsuario.tsx
@@ -2,10 +2,9 @@ import { StyleSheet } from 'react-native';
 
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: '#220641', padding: 24, textAlign: 'center', alignItems: 'center'},
+  title: { color: '#fff', fontSize: 28, fontWeight: 'bold', marginBottom: 8 },
+  subtitle: { color: '#fff', fontSize: 16, marginBottom: 24 },
+  input: { marginBottom: 24, backgroundColor: '#f5f5f5', width: '100%', textDecorationColor: '#fff' },
+  button: { paddingVertical: 6 },
 });
